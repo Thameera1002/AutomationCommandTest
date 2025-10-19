162d768443ef5bbfdc4b3f115ef5ed454a71d0d4
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-10-19 20:31:47 +0530

    TestNGAnnotationOrder.java added

---
+++ src/test/java/com/testng/TestNGAnnotationOrder.java
@@ -0,0 +1,45 @@
+package com.testng;
+
+import org.testng.annotations.*;
+
+public class TestNGAnnotationOrder {
+
+    @Test
+    public void testMethod() {
+        System.out.println("TestMethod");
+    }
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("beforeSuite");
+    }
+    @BeforeTest
+    public void beforeTest() {
+        System.out.println("beforeTest");
+    }
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("beforeClass");
+    }
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("beforeMethod");
+    }
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("afterMethod");
+    }
+    @AfterClass
+    public void afterClass() {
+        System.out.println("afterClass");
+    }
+    @AfterTest
+    public void afterTest() {
+        System.out.println("afterTest");
+    }
+
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("afterSuite");
+    }
+}
