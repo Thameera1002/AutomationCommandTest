d9ec90b50e11c1e220f4eff3d5fe5584df3a88b4
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2025-11-10 19:35:53 +0530

    AssertionTestEx.java added

---
+++ src/test/java/com/testng/AssertionTestEx.java
@@ -0,0 +1,31 @@
+package com.testng;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AssertionTestEx {
+    @Test
+    public void testMethod1() {
+        System.out.println("A");
+        System.out.println("B");
+        Assert.assertTrue(false);
+        System.out.println("C");
+        System.out.println("D");
+    }
+
+    @Test
+    public void testMethod2() {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.get("http://31.220.75.248:3000/");
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Library Management System - Admin Portal";
+        Assert.assertEquals(actualTitle, expectedTitle);
+        boolean displayed = driver.findElement(By.xpath("//span[text()='Library Portal']")).isDisplayed();
+        Assert.assertTrue(displayed);
+        System.out.println("Checkpoint reached");
+        driver.quit();
+    }
+}
